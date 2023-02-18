8d72f23bb78126df305fdb54435f0e75885b85e7
collection <lakshmipathij666@gmail.com>
2023-02-18 12:03:50 +0530

    for

--- Palindrome.java
+++ Palindrome.java
@@ -3,16 +3,17 @@ package For;
 public class Palindrome {
 	public static void main(String[] args) {
 		String a="Madam";
+		int x=10;
 		String temp="";
 		for(int i=a.length()-1;i>=0;i--) {
 			temp=temp+a.charAt(i);
 			
 		}
 		if(a.equalsIgnoreCase(temp)){
-			System.out.println("its a palindrome");
+			System.out.println("palindrome");
 		}
 		else {
-			System.out.println("its not a palindrome");
+			System.out.println("no its not a palindrome");
 		}
 	}
 
